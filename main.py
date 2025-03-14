05f05728dff25b70a98343dc805bebe18f6616d6
WuJiXing <mongol.kun@gmail.com>
2025-03-14 22:22:55 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,44 @@
+import os
+from contextlib import asynccontextmanager
+from http import HTTPStatus
+from dotenv import load_dotenv
+from fastapi import FastAPI, Request, Response
+from telegram import Update
+from telegram.ext import Application
+from db import connect_db
+from commands import register_handlers
+
+# Load environment variables
+load_dotenv()
+TELEGRAM_BOT_TOKEN = os.getenv('TELEGRAM_BOT_TOKEN')
+WEBHOOK_DOMAIN = os.getenv('RAILWAY_PUBLIC_DOMAIN')
+
+# Build the Telegram Bot application
+bot_builder = (
+    Application.builder()
+    .token(TELEGRAM_BOT_TOKEN)
+    .updater(None)
+    .build()
+)
+
+@asynccontextmanager
+async def lifespan(_: FastAPI):
+    """ Sets the webhook for the Telegram Bot and manages its lifecycle (start/stop). """
+    await bot_builder.bot.setWebhook(url=WEBHOOK_DOMAIN)
+    async with bot_builder:
+        await bot_builder.start()
+        yield
+        await bot_builder.stop()
+
+app = FastAPI(lifespan=lifespan)
+
+@app.post("/")
+async def process_update(request: Request):
+    """ Handles incoming Telegram updates and processes them with the bot. """
+    message = await request.json()
+    update = Update.de_json(data=message, bot=bot_builder.bot)
+    await bot_builder.process_update(update)
+    return Response(status_code=HTTPStatus.OK)
+
+# Регистрируем команды
+register_handlers(bot_builder)
