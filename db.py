49008cedbd48cf55502759d7d4afc80f9c9438a9
WuJiXing <mongol.kun@gmail.com>
2025-03-15 01:54:32 +0300

    Update db.py

--- db.py
+++ db.py
@@ -3,6 +3,9 @@ import os
 from dotenv import load_dotenv
 
 load_dotenv()
+HELP_TABLE = os.getenv("HELP_TABLE", "help")
+INFO_TABLE = os.getenv("INFO_TABLE", "info")
+POST_TABLE = os.getenv("POST_TABLE", "post")
 
 async def connect_db():
     """Подключение к PostgreSQL"""
