d8f3c8e2c09cad1da4773b47f88e85ce87faa6b2
WuJiXing <mongol.kun@gmail.com>
2025-03-14 23:44:16 +0300

    Update db.py

--- db.py
+++ db.py
@@ -1,8 +1,10 @@
 import asyncpg
 import os
 
-
+print("📡 Загружается db.py...")  # Проверяем, вызывается ли вообще этот файл
 
 async def connect_db():
     """ Подключение к PostgreSQL """
+    print("📡 Подключаемся к базе данных...")  # Должно появиться в логах Railway
     return await asyncpg.create_pool(os.getenv('DATABASE_URL'))
+    print(f"📡 DATABASE_URL: {os.getenv('DATABASE_URL')}")
