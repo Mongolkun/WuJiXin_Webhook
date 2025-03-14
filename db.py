79fc643362196f030b58494154b8ea1d8b74e799
WuJiXing <mongol.kun@gmail.com>
2025-03-15 01:30:43 +0300

    Update db.py

--- db.py
+++ db.py
@@ -11,4 +11,3 @@ POST_TABLE = os.getenv("POST_TABLE", "post")
 async def connect_db():
     """Подключение к PostgreSQL"""
     database_url = os.getenv('DATABASE_URL')
-    return await asyncpg.create_pool(database_url, min_size=1, max_size=5)
