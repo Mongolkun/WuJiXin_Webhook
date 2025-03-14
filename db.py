8d24c38eba995005529275af396b491ab22cb148
WuJiXing <mongol.kun@gmail.com>
2025-03-15 00:43:59 +0300

    Update db.py

--- db.py
+++ db.py
@@ -8,10 +8,6 @@ HELP_TABLE = os.getenv("HELP_TABLE", "help")
 INFO_TABLE = os.getenv("INFO_TABLE", "info")
 POST_TABLE = os.getenv("POST_TABLE", "post")
 
-print("📡 Загружается db.py...")  # Проверяем, вызывается ли вообще этот файл
-
 async def connect_db():
     """ Подключение к PostgreSQL """
-    print("📡 Подключаемся к базе данных...")  # Должно появиться в логах Railway
-    print(f"📡 DATABASE_URL: {os.getenv('DATABASE_URL')}")
     return await asyncpg.create_pool(os.getenv('DATABASE_URL'))
