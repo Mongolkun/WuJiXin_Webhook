9277162211bdc397a182600f263c8e437fbcf1d5
WuJiXing <mongol.kun@gmail.com>
2025-03-14 23:42:10 +0300

    Update db.py

--- db.py
+++ db.py
@@ -6,8 +6,3 @@ import os
 async def connect_db():
     """ Подключение к PostgreSQL """
     return await asyncpg.create_pool(os.getenv('DATABASE_URL'))
-
-# Команда /start
-async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    """Приветствие при запуске бота"""
-    await update.message.reply_text("Привет! Это WuJiXing Telegram Bot 🚀")
